3ead7792f91c79cf25f752f25fe57adbd3240f4f
richpeters <rpeter@sandia.gov>
2024-02-09 09:39:30 -0800

    cleanup and more docs

--- README.md
+++ README.md
@@ -4,20 +4,32 @@ This is a demo of being unable to load webdrivers.
 the repo is git@github.com:rpii/wdio-webdriver-test.git
 
 ## Setup
-node-modules is populated in the zip file,  just run if on windows.
+run yarn install
 
 yarn test:chrome
+yarn test:chrome-headless
+yarn test:chrome-headless-linux
 yarn test:edge
+yarn test:edge-headless
+yarn test:edge-headless-linux
 yarn test:firefox
-etc
+yarn test:firefox-headless
+yarn test:firefox-headless-linux
+
 
 firefox is the only one which works.
 
 
 ## Output
 log files containing the failure of connecting to the webdriver.
+in the ./logs folder
 
 use yarn clean-logs prior to a run
+
+## Linux Setup for Oracle Linux 8.8
+    yum install google-chrome
+    yum install firefox
+    yum install microsoft-edge-stable
 
 
 
--- src/test/Component.test.ts
+++ src/test/Component.test.ts
@@ -1,9 +1,26 @@
 import { $, expect } from '@wdio/globals'
+import ReportEvents from "@rpii/wdio-report-events" ;
+import * as path from "path";
+import fs from "fs-extra";
+let eventReporter = new ReportEvents() ;
 
+async function logMessage(message:string) {
+    await eventReporter.logMessage( message)
+}
+
+async function takeScreenshot(message:string) {
+    fs.ensureDirSync('reports/html-reports/screenshots/');
+    const filepath = path.join('reports/html-reports/screenshots/', 'google.png');
+    await browser.saveScreenshot(filepath);
+    await logMessage(message);
+    eventReporter.logScreenshot(filepath) ;
+}
 describe('WebdriverIO Webdriver Testing', () => {
     it('should be able to get the google home page title', async () => {
         await browser.url("/") ;
         await expect($("//title")).toBePresent();
         // await expect($("//title")).toHaveText("Google") ;
+        await takeScreenshot("Navigated to Google") ;
     })
 })
+
